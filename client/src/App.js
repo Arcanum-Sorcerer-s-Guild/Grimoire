627f606cf332363ca0379892b61ffac0a02079ca
jasonmartin85 <JasonMartin85@gmail.com>
2023-03-25 08:49:27 -0400

    Added PostEntry Component and path to /post

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Entries from "./Home/Entries.js";
 import Login from "./Login/Login.js"
 import NavBar from "./Common/NavBar.js"
+import PostEntry from "./PostEntry/PostEntry.js"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route exact path = "/home" element={<Entries />} />
         {/* <Route path = "/templates" element={<Templates />} /> */}
         <Route path="/login" element={<Login />} />
+        <Route path="/post" element={<PostEntry />} />
       </Routes>
     </div>
   );
---
+++ client/src/PostEntry/PostEntry.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostEntry = () => {
+  return(
+    <div>
+      Hey!
+    </div>
+  )
+}
+
+export default PostEntry;
