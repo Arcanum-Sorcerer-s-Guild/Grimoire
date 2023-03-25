d9ecccc5a6b357b80e4932aa490d4b7c6b3ac937
Jacob S. Steward <jacobtheeldest@gmail.com>
2023-03-24 22:38:19 -0400

    switch description from string to text datatype. strings have a 255 char limit.

--- server/migrations/20230324154631_create_tables.js
+++ server/migrations/20230324154631_create_tables.js
@@ -11,7 +11,7 @@ exports.up = function(knex) {
     .createTable('entries', (table) => {
       table.increments('id').primary();
       table.string('title').notNullable();
-      table.string('description').notNullable();
+      table.text('description').notNullable();
       table.datetime('created', { useTz: false, precision: 3 }).defaultTo(knex.fn.now());
       table.datetime('updated', { useTz: false, precision: 3 }).defaultTo(knex.fn.now());
       table.integer('user_id').unsigned().notNullable();
