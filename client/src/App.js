9d5338f4538a0b85a6723e469ebea60bc954a2e5
ddbonilla <ddbonillav@gmail.com>
2023-03-26 12:37:29 -0700

    dark mode theme component (togglable) created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
 import React, {useState, useEffect} from 'react'
+import { Routes, Route } from "react-router-dom";
 import Entries from "./Home/Entries.js";
 import Login from "./Login/Login.js"
+import Theme from "./Common/Theme.js"
 import NavBar from "./Common/NavBar.js"
 import PostEntry from "./PostEntry/PostEntry.js"
-import { Routes, Route } from "react-router-dom";
+import './App.css';
 
 //require("dotenv").config();
 
@@ -28,7 +29,8 @@ function App() {
 
   return (
     <mslContext.Provider value={ {srvPort, databaseTags, searchTerms, setSearchTerms} }>
-      <div>
+      <section className="min-h-screen duration-100 dark:text-gray-100 dark:bg-slate-900">
+      <Theme />
       <NavBar />
       <Routes>
         <Route path = "/home" element = {<Entries />} />
@@ -37,7 +39,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/post" element={<PostEntry />} />
       </Routes>
-      </div>
+      </section>
     </mslContext.Provider>
   )
 }
