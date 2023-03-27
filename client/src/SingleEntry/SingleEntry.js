873b540fe8f096f5c5628c44e18a4391a73518f7
JasonMartin85 <jason.martin.85@gmail.com>
2023-03-27 17:41:05 -0400

    Updated Single Entry to show details of individual entry

--- client/src/SingleEntry/SingleEntry.js
+++ client/src/SingleEntry/SingleEntry.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react'
+import React,{useEffect, useState} from 'react'
 import { mslContext } from "../App.js"
 import {useParams} from 'react-router-dom'
 
@@ -6,19 +6,25 @@ import {useParams} from 'react-router-dom'
 const SingleEntry = () => {
   let params = useParams()
   const { srvPort } = React.useContext(mslContext);
+  const [entry, setEntry] = useState({})
 
 
-  // useEffect(
-  //   fetch(`http://localhost:${srvPort}/entries?id=${params.id}`)
-  //   .then(res => res.json())
-  //   .then(data => {
-  //     console.log(data)
-  //   })
-  //   ,[])
+  useEffect(() => {
+    fetch(`http://localhost:${srvPort}/entries?id=${params.id}`)
+    .then(res => res.json())
+    .then(data => {
+      setEntry(data.data[0])
+      console.log(entry)
+      
+    })
+  },[])
 
   return(
     <div>
-      {params.id}
+    {`Created: ${entry.title} by ${entry.user} on ${entry.created}`}<br/>
+    {`Updated: ${entry.updated}` }<br/>
+    <p>{entry.description}</p>
+
     </div>
   )
 }
