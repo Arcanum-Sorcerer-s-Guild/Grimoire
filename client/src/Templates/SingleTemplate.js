67ecbcf69bd3c9ec6625aa542185c9186fd2cc46
JasonMartin85 <jason.martin.85@gmail.com>
2023-03-29 09:04:34 -0400

    Updated templates page to use accordion view

--- client/src/Templates/SingleTemplate.js
+++ client/src/Templates/SingleTemplate.js
@@ -1,81 +1,87 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 import { Spinner, Button } from "flowbite-react";
 import { useParams, useNavigate } from "react-router-dom";
 import { mslContext } from "../App.js";
 
-
 const SingleTemplate = () => {
-  const { srvPort, templateValues, setTemplateValues } = React.useContext(mslContext);
+  const { srvPort, templateValues, setTemplateValues } =
+    React.useContext(mslContext);
   const [template, setTemplate] = useState();
   const navigate = useNavigate();
 
   const selectTemplate = () => {
-    let description1 = "SLKDJfsjd"
-    let title1 = "laksjdfls"
-    setTemplateValues( {
+    let description1 = "SLKDJfsjd";
+    let title1 = "laksjdfls";
+    setTemplateValues({
       description: description1,
-      title: title1
-    })
-    navigate('/post')
-  }
+      title: title1,
+    });
+    navigate("/post");
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch(`http://localhost:${srvPort}/templates`)
-    .then(res=>res.json())
-    .then(data=>{
-      console.log(data)
-      setTemplate(data.data)
-    })
-  },[])
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setTemplate(data.data);
+      });
+  }, []);
 
-  return(<>
-    <section className="col-span-2 place-items-center max-h-fit w-full mb-5">  
-      <div className="px-9">
-        {template 
-        ? <div>Templates loaded!</div>
-        : <Spinner aria-label="Extra large spinner example" size="xl" /> }
-      
-    <div class="overflow-hidden bg-white shadow sm:rounded-lg">
-      <div class="px-4 py-5 sm:px-6">
-        <h3 class="text-base font-semibold leading-6 text-gray-900">Template ###</h3>
-        <p class="mt-1 max-w-2xl text-sm text-gray-500">Template created: ###</p>
-      </div>
-      <div class="border-t border-gray-200"></div>
-    <dl>
-      <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-500">Title</dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">title</dd>
-      </div>
-      <div class="border-t border-gray-200"></div>
-      <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-        <dt class="text-sm font-medium text-gray-500">Description</dt>
-        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Description</dd>
-      </div>
-      <div>
-        <dd>
-          <ul>
-          </ul>
-        </dd>
-      </div>
-    </dl>
-  </div>
+  return (
+    <>
+      <section className="col-span-2 place-items-center max-h-fit w-full mb-5">
+        <div className="px-9">
+          {template ? (
+            <>
+              <div class="overflow-hidden bg-white shadow sm:rounded-lg">
+                <div class="px-4 py-5 sm:px-6">
+                  <h3 class="text-base font-semibold leading-6 text-gray-900">
+                    Template ###
+                  </h3>
+                  <p class="mt-1 max-w-2xl text-sm text-gray-500">
+                    Template created: ###
+                  </p>
+                </div>
+                <div class="border-t border-gray-200"></div>
+                <dl>
+                  <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">Title</dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                      title
+                    </dd>
+                  </div>
+                  <div class="border-t border-gray-200"></div>
+                  <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">
+                      Description
+                    </dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                      Description
+                    </dd>
+                  </div>
+                  <div>
+                    <dd>
+                      <ul></ul>
+                    </dd>
+                  </div>
+                </dl>
+              </div>
 
+              <div className="flex justify-center pt-10">
+                {" "}
+                <Button onClick={() => selectTemplate()}>
+                  Use Template
+                </Button>{" "}
+              </div>
+            </>
+          ) : (
+            <Spinner aria-label="Extra large spinner example" size="xl" />
+          )}
+        </div>
+      </section>
+    </>
+  );
+};
 
-
-
-
-  <div className="flex justify-center pt-10"> <Button onClick={() => selectTemplate()}>Use Template</Button> </div>
-        
-
-    
-    </div>
-    </section>
-
-  </>)
-
-
-
-}
-
-
-export default SingleTemplate
+export default SingleTemplate;
--- client/src/Templates/Templates.js
+++ client/src/Templates/Templates.js
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { mslContext } from "../App.js";
 import { Pagination, Spinner } from "flowbite-react";
+import AccordionItem from "../Common/AccordionItem";
+import DateObject from "react-date-object";
+import { Link } from "react-router-dom";
 
 
 const Templates = () => {
   const { srvPort } = React.useContext(mslContext);
   const [templates, setTemplates] = useState();
+  const [open, setOpen] = useState(false);
+
+  const toggle = (index) => {
+    if (open === index) {
+      return setOpen(false);
+    }
+    setOpen(index);
+  };
 
   useEffect(()=>{
     fetch(`http://localhost:${srvPort}/templates`)
@@ -20,10 +31,30 @@ const Templates = () => {
     <section className="col-span-2 place-items-center max-h-fit w-full mb-5">  
       <div className="px-9">
         {templates 
-        ? <div>Templates loaded!</div>
-        : <Spinner aria-label="Extra large spinner example" size="xl" /> }
-        <div>Multiple entries</div>
+        ? <div>
+                {templates.map((entry, index) => {
+                  let title = entry.title.toUpperCase();
+                  let dateCreated = new DateObject(entry.created).format(
+                    "YYYY-MM-DD  HH:mm"
+                  );
 
+                  return (
+                    <AccordionItem
+                      key={index}
+                      open={index === open}
+                      title={<Link to={`/Templates/${entry.id}`}>{`${title}`}</Link>}
+                      dateCreated={`${dateCreated}`}
+                      desc={entry.description}
+                      toggle={() => toggle(index)}
+                      user={entry.user}
+                    />
+                  );
+                })}{" "}
+              </div>
+        : <Spinner aria-label="Extra large spinner example" size="xl" /> }
+
+
+        
 
 
 
