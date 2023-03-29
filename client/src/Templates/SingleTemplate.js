af0da42865d5c07dc37ec7d64a172a6a140aaeed
JasonMartin85 <jason.martin.85@gmail.com>
2023-03-29 08:57:32 -0400

    Updated the look of single template view to more in line with rest of website

--- client/src/Templates/SingleTemplate.js
+++ client/src/Templates/SingleTemplate.js
@@ -34,9 +34,37 @@ const SingleTemplate = () => {
         {template 
         ? <div>Templates loaded!</div>
         : <Spinner aria-label="Extra large spinner example" size="xl" /> }
-        <div>Single Entry</div>
+      
+    <div class="overflow-hidden bg-white shadow sm:rounded-lg">
+      <div class="px-4 py-5 sm:px-6">
+        <h3 class="text-base font-semibold leading-6 text-gray-900">Template ###</h3>
+        <p class="mt-1 max-w-2xl text-sm text-gray-500">Template created: ###</p>
+      </div>
+      <div class="border-t border-gray-200"></div>
+    <dl>
+      <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+        <dt class="text-sm font-medium text-gray-500">Title</dt>
+        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">title</dd>
+      </div>
+      <div class="border-t border-gray-200"></div>
+      <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+        <dt class="text-sm font-medium text-gray-500">Description</dt>
+        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Description</dd>
+      </div>
+      <div>
+        <dd>
+          <ul>
+          </ul>
+        </dd>
+      </div>
+    </dl>
+  </div>
 
-        <Button onClick={() => selectTemplate()}>Use Template</Button>
+
+
+
+
+  <div className="flex justify-center pt-10"> <Button onClick={() => selectTemplate()}>Use Template</Button> </div>
         
 
     
