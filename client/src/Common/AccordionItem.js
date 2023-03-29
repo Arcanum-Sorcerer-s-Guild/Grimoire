00eff2504c6b637e928f33af734a4b55d69edb46
JasonMartin85 <jason.martin.85@gmail.com>
2023-03-29 00:25:53 -0400

    Added pagination to Entries.js

--- client/src/Common/AccordionItem.js
+++ client/src/Common/AccordionItem.js
@@ -1,7 +1,7 @@
 import { Collapse } from 'react-collapse';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
-const AccordionItem = ({open, toggle, title, date, desc}) => {
+const AccordionItem = ({open, toggle, title, dateCreated, dateUpdated, desc, tags, user}) => {
     return (
         <div className="pt-2 shadow-md">
             <div
@@ -12,10 +12,10 @@ const AccordionItem = ({open, toggle, title, date, desc}) => {
                 <p className="text-xl  text-blue-800 dark:text-amber-500">
                     {title}
                     <span className={`${open && "hidden"} text-sm m-2 text-gray-500 dark:text-gray-400`}>by "username" on</span>
-                    <span className={`${open && "hidden"} text-sm text-gray-500 dark:text-gray-400`}>{date}</span>
+                    <span className={`${open && "hidden"} text-sm text-gray-500 dark:text-gray-400`}>{dateCreated}</span>
                 </p>
                 <div className="flex text-sm">
-                    <div className={`mr-5 ${open && "hidden"}`}>tag tag tag</div>
+                    <div className={`mr-5 ${open && "hidden"}`}>{`${tags}`}</div>
                     <div>
                          { open ? <AiOutlineMinus /> : <AiOutlinePlus /> }
                     </div>
@@ -24,12 +24,12 @@ const AccordionItem = ({open, toggle, title, date, desc}) => {
             <Collapse isOpened={open}>
             <div className="text-sm px-5 pb-5
                  bg-slate-200 text-gray-700 dark:text-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400">
-                    <p className={`${!open && "hidden"} text-sm mb-2 text-gray-500 dark:text-gray-400`}>by "username"
-                        <span className={`${!open && "hidden"} text-sm ml-2 text-gray-500 dark:text-gray-400 `}>created: {date}</span>
-                        <span className={`${!open && "hidden"} text-sm ml-2 text-gray-500 dark:text-gray-400 `}>updated: {date}</span>
+                    <p className={`${!open && "hidden"} text-sm mb-2 text-gray-500 dark:text-gray-400`}>{`by ${user}`}
+                        <span className={`${!open && "hidden"} text-sm ml-2 text-gray-500 dark:text-gray-400 `}>created: {dateCreated}</span>
+                        {dateCreated !== dateUpdated ? <span className={`${!open && "hidden"} text-sm ml-2 text-gray-500 dark:text-gray-400 `}>updated: {dateUpdated}</span> : <span></span>}
                     </p>
                     {desc}
-                    <div className={`mt-5 ${!open && "hidden"}`}>tag tag tag</div>
+                    <div className={`mt-5 ${!open && "hidden"}`}>{`${tags}`}</div>
                 </div>
             </Collapse>
         </div>
--- client/src/Home/Entries.js
+++ client/src/Home/Entries.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { mslContext } from "../App.js";
 import AccordionItem from "../Common/AccordionItem";
+import { Pagination } from "flowbite-react";
 import DateObject from "react-date-object";
-import {Link} from "react-router-dom"
+import {Link} from "react-router-dom";
 
 const Entries = () => {
   const [entries, setEntries] = useState([]);
@@ -10,6 +11,15 @@ const Entries = () => {
     React.useContext(mslContext);
 
   const [open, setOpen] = useState(false);
+  const [pageData, setPageData] = useState()
+  
+  const onPageChange = (value) => {
+    console.log(value)
+    
+    setSearchTerms({...searchTerms,page:value })
+
+    console.log(pageData.currentPage,searchTerms)
+  }
 
   const toggle = (index) => {
     if (open === index) {
@@ -22,12 +32,16 @@ const Entries = () => {
     let searchTerm = "";
     if (searchTerms.q !== undefined) searchTerm += `q=${searchTerms.q}`;
     if (searchTerms.tags !== undefined) searchTerm += `&${searchTerms.tags}`;
-
+    if (searchTerms.page !== undefined) searchTerm = `&page=${searchTerms.page}`
+    console.log(searchTerm)
     fetch(`http://localhost:${srvPort}/entries?${searchTerm}`)
       .then((res) => res.json())
       .then((data) => {
         if (data.data !== null) {
           setEntries(data.data)
+          console.log(data.pagination)
+          setPageData(data.pagination)
+          
         } else {
           setEntries(undefined);
         }
@@ -43,17 +57,20 @@ const Entries = () => {
               <div>
                 {entries.map((entry, index) => {
                   let title = entry.title.toUpperCase();
-                  let date = new DateObject(entry.created);
-                  let dateCreated = date.format("YYYY-MM-DD");
+                  let dateCreated = new DateObject(entry.created).format("YYYY-MM-DD HH:mm");
+                  let dateUpdated = new DateObject(entry.updated).format("YYYY-MM-DD HH:mm")
 
                   return (
                     <AccordionItem
                       key={index}
                       open={index === open}
                       title={<Link to={`/Home/${entry.id}`}>{`${title}`}</Link>}
-                      date={`${dateCreated}`}
+                      dateCreated={`${dateCreated}`}
+                      dateUpdated={`${dateUpdated}`}
                       desc={entry.description}
                       toggle={() => toggle(index)}
+                      tags={entry.tags}
+                      user={entry.user}
                     />
                   );
                 })}{" "}
@@ -62,6 +79,17 @@ const Entries = () => {
               <div>Loading</div>
             )}
           </div>
+          <div className="flex items-center justify-center text-center">
+          {pageData ? <Pagination
+                    currentPage={parseInt(pageData.currentPage)}
+                    layout="pagination"
+                    onPageChange={onPageChange}
+                    showIcons={true}
+                    totalPages={parseInt(pageData.lastPage)}
+                    previousLabel="Go back"
+                    nextLabel="Go forward"
+                  /> : <div>Loading</div>}
+            </div>
         </div>
       </section>
     </>
