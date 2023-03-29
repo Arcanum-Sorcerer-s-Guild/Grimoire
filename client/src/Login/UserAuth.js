7d1026fe1d24d4201d47dc6235c05a985ab4cd33
Jacob S. Steward <jacobtheeldest@gmail.com>
2023-03-29 15:27:49 -0400

    Remove logout button from login page

--- client/src/Login/UserAuth.js
+++ client/src/Login/UserAuth.js
@@ -29,14 +29,14 @@ const UserAuth = () => {
   };
 
   // onClick handler for logging out a user
-  const handleLogout = () => {
-    fetch(`http://localhost:${srvPort}/logout`, {
-      method: "POST",
-      credentials: "include",
-    }).then(() => {
-      setUser({});
-    });
-  };
+  // const handleLogout = () => {
+  //   fetch(`http://localhost:${srvPort}/logout`, {
+  //     method: "POST",
+  //     credentials: "include",
+  //   }).then(() => {
+  //     setUser({});
+  //   });
+  // };
 
   // onClick handler for retrieving a user's data
   // response is:
@@ -133,21 +133,12 @@ const UserAuth = () => {
                   </div>
                 </div>
                 <div>
-                  {!user.username ? (
-                    <button
-                      type="submit"
-                      className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-amber-600 hover:bg-amber-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-400 mt-5"
-                    >
-                      Login
-                    </button>
-                  ) : (
-                    <button
-                      onClick={handleLogout}
-                      className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-amber-600 hover:bg-amber-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-400 mt-5"
-                    >
-                      Logout
-                    </button>
-                  )}
+                  <button
+                    type="submit"
+                    className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-amber-600 hover:bg-amber-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-amber-400 mt-5"
+                  >
+                    Login
+                  </button>
                 </div>
               </div>
             </form>
